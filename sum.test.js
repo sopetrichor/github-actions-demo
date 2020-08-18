c56277b22a8bf6f1b6663b106a6462a1051465a3
sopetrichor <stcwer@gmail.com>
2020-08-18 23:45:38 +0800

    add demo script and unit test

---
+++ sum.test.js
@@ -0,0 +1,10 @@
+const sum = require('./sum');
+
+describe('sum', () => {
+    it('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+    it('adds 7 + 2 to equal 9', () => {
+        expect(sum(7, 2)).toBe(10);
+    });
+});
