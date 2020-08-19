13454d6f3f36720f9ba8da7039b7c1b0134dfd5e
Benny Hsieh <stcwer@gmail.com>
2020-08-19 12:16:26 +0800

    Update sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -5,7 +5,7 @@ describe('sum', () => {
         expect(sum(1, 2)).toBe(3);
     });
     it('adds 7 + 2 to equal 9', () => {
-        expect(sum(7, 2)).toBe(10);
+        expect(sum(7, 2)).toBe(9);
     });
     it('adds 3 + 6 to equal 9', () => {
         expect(sum(3, 6)).toBe(9);
