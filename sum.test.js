39b651510606e19f17cd28f265e24f12aa3003c0
Benny Hsieh <stcwer@gmail.com>
2020-08-19 22:36:27 +0800

    Update sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -1,11 +1,11 @@
 const sum = require("./sum");
 
 describe("sum", () => {
-  it("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
+  it("adds 1 + 4 to equal 5", () => {
+    expect(sum(1, 4)).toBe(5);
   });
-  it("adds 7 + 2 to equal 9", () => {
-    expect(sum(7, 2)).toBe(9);
+  it("adds 2 + 5 to equal 7", () => {
+    expect(sum(2, 5)).toBe(7);
   });
   it("adds 3 + 6 to equal 9", () => {
     expect(sum(3, 6)).toBe(9);
