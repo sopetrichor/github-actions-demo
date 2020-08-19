e43ac5c118f7e75db0e7a1a6877520196ca62fc9
sopetrichor <sopetrichor@users.noreply.github.com>
2020-08-19 09:31:49 +0000

    Apply formatting changes

--- sum.test.js
+++ sum.test.js
@@ -1,13 +1,13 @@
-const sum = require('./sum');
+const sum = require("./sum");
 
-describe('sum', () => {
-    it('adds 1 + 2 to equal 3', () => {
-        expect(sum(1, 2)).toBe(3);
-    });
-    it('adds 7 + 2 to equal 9', () => {
-        expect(sum(7, 2)).toBe(9);
-    });
-    it('adds 3 + 6 to equal 9', () => {
-        expect(sum(3, 6)).toBe(9);
-    });
+describe("sum", () => {
+  it("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+  it("adds 7 + 2 to equal 9", () => {
+    expect(sum(7, 2)).toBe(9);
+  });
+  it("adds 3 + 6 to equal 9", () => {
+    expect(sum(3, 6)).toBe(9);
+  });
 });
